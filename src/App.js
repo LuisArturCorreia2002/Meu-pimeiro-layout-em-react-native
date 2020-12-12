6db63237e0566470ab388f39bcaaa45c6977c36e
Escanor404 <49661134+Escanor404@users.noreply.github.com>
2020-12-11 22:49:46 -0300

    Tem erro
    Vou resolver o erro amanhã

--- src/App.js
+++ src/App.js
@@ -1,25 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import react, {useState, useEffect} from 'react';
+import {View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Text, StyleSheet, Animated, Keyboard} from 'react-native';
 
-function App() {
+export default function app() {
+
+  const [offset] = useState(new Animated.ValueXY({x: 0, y: 95}));
+  const [opacity] = useState(new Animated.Value(0));
+  const [logo] = useState(new Animated.ValueXY({x: 130, y: 155}));
+
+  useEffect(() => {
+
+      keyboardDidShowListener = key.addListener('keyboardDidShow', keyboardDidShow);
+      keyboardDidHideListener = key.addListener('keyboardDidHide', keyboardDidHide);
+
+    Animated.parallel([
+      Animated.spring(offset.y, {
+        toValue: 0,
+        speed: 4,
+        bounciness: 20
+      }),
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 200
+      })
+    ]).start();
+  
+  }, []);
+
+  function keyboardDidShow(){
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 55,
+        duration: 100
+      }),
+      Animated.parallel([
+        Animated.timing(logo.y, {
+          toValue: 65,
+          duration: 100
+        })
+    ])
+  };
+
+  function keyboardDidHide(){
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 130,
+        duration: 100
+      }),
+      Animated.parallel([
+        Animated.timing(logo.y, {
+          toValue: 155,
+          duration: 100
+        })
+      };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.containerLogo}>
+        <Animated.Image 
+        style={{
+          width: logo.x,
+          height: logo.y
+        }}
+        source={require('./src/assets/logo')}
+        />
+      </View>
+      <Animated.View style={[
+        styles.container,
+        {
+          opacity: opacity,
+          transform: [
+            {translateY: offset.y}
+          ]
+        }
+      ]}>
+        <TextInput
+        style={styles.input}
+        placeholder="Email"
+        autroCorrect={false}
+        onChangeText={()=> {}}
+        />
+        <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        autroCorrect={false}
+        onChangeText={()=> {}}
+        />
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.submitText}>Acessar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnRegister}>
+          <Text style={styles.registerText}>Criar conta gratuita</Text>
+        </TouchableOpacity>
+      </Animated.View>
+  </KeyboardAvoidingView>  
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  background:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#191919'
+  },
+  containerLogo:{
+    flex:1,
+    justifyContent: 'center'
+  },
+  container:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    paddingBottom: 50
+  },
+  input:{
+    backgroundColor: '#fff',
+    width: '90%',
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 7,
+    padding: 10
+  },
+  btnSubmit:{
+    color: '#35AAFF',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7
+  },
+  submitText:{
+    color: '#fff',
+    fontSize: 18
+  },
+  btnRegister:{
+    marginTop: 10
+  },
+  registerText:{
+    color: '#fff'
+  }
+});
